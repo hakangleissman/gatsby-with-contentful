a004837c3cdeda872d4c3fcb496f92b904c19c93
hakangleissman <hakan.gleissman@gmail.com>
2023-12-11 16:54:37 +0100

    removed contentful stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,7 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+
 
 module.exports = {
   siteMetadata: {
@@ -28,15 +26,7 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-contentful`,
-      options:  {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    },
+    `gatsby-plugin-sharp`, 
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
